f61c1c4362d2c99ff52273e0b6f78f13d36eeae8
Carrie <carriearoberts95@gmail.com>
2021-06-03 10:52:49 +0100

    Newest changes

---
+++ challenges/0-check-if-equal.js
@@ -0,0 +1,13 @@
+
+
+function isEqual(a,b) {
+    if (a === b) {
+        return true
+    } else {
+        if (a !== b)
+        return false
+    }
+}
+
+// console.log(isEqual);
+console.log("isEqual(5, 6):", isEqual(5, 6), "should be False");
