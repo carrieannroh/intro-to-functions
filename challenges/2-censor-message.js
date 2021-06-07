197e31c684c933c199f1cdebececff98d346fe02
Carrie <carriearoberts95@gmail.com>
2021-06-07 10:36:45 +0100

    committing changes to 2-censor exercise

--- challenges/2-censor-message.js
+++ challenges/2-censor-message.js
@@ -1,3 +1,12 @@
 function censorMessage() {
+    let message = "Here is a really nice beautiful message"
+    let banList = "nice"
+
+    if (banList in message) {
+
+    return message.pop
+    }
 
 }
+
+console.log(censorMessage());
